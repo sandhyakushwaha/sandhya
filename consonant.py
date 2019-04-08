303e37efed7d746ac97220a7d1dd6f3cda3ba405
sandhyakushwaha <45137234+sandhyakushwaha@users.noreply.github.com>
2019-04-08 19:59:26 +0530

    Add files via upload

---
+++ consonant.py
@@ -0,0 +1,7 @@
+c=input("Enter character")
+if c=='a'or c=="i"or c=='e'or c=='o' or c==" u" or c=="A" or c=="E" or c=="I" or c=="O" or c=="U" :
+    print("Vowel")
+elif c=='[' or c=='@' or c=='_' or c=='!' or c=='#' or c=='$' or c=='%' or c=='^' or c=='&' or c=='*' :
+    print("invalid")
+else:
+    print("Consonant")
