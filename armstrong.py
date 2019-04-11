60d347c466ca09adc0730858a10521b1e65b42d9
sandhyakushwaha <45137234+sandhyakushwaha@users.noreply.github.com>
2019-04-12 00:01:34 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,13 @@
+n = int(input())
+order = len(str(n))
+sum = 0
+temp = n
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+
+if n == sum:
+   print("yes")
+else:
+   print("no")
