557e7c76e9bb7ba0d4fd04a6729d596db23b5697
sandhyakushwaha <45137234+sandhyakushwaha@users.noreply.github.com>
2019-04-19 22:15:30 +0530

    Add files via upload

---
+++ smallestElement.py
@@ -0,0 +1,14 @@
+data=[]
+list=[]
+n=int(input())
+for i in range(1,n+1):
+    ele=int(input())
+    list.append(ele)
+while list:
+    min=list[0]
+    for i in list:
+        if i<min:
+            min=i
+    data.append(min)
+    list.remove(min)
+print(data[0]) 
